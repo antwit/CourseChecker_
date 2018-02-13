939a8fadf98e00046171028d6f2c4a8e4ca80b61
Anton Witt <a.witt@ids-system.de>
2018-02-13 07:57:31 +0100

    Initial

---
+++ ConsoleApp1/ConsoleApp1/WPF/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CourseChecker.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : UserControl
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
